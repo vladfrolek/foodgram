80d03e46a5af153af121e46d622525108f91e3b1
Vlad Frolek <vladfrolek@gmail.com>
2024-10-12 15:09:40 +0300

    2nd

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-
 from constants import USER_MODEL_MAX_LENGTH
 
 
