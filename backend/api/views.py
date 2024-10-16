12f665ad3a4decc65227cb3f391c1fe8304372ff
Vlad Frolek <vladfrolek@gmail.com>
2024-10-16 12:36:53 +0300

    5th

--- backend/api/views.py
+++ backend/api/views.py
@@ -162,8 +162,8 @@ class TagViewset(viewsets.ReadOnlyModelViewSet):
 
 
 class IngredientViewset(viewsets.ReadOnlyModelViewSet):
-    filter_backends = [SearchFilter]
-    search_fields = ['^name']
+    filter_backends = (SearchFilter,)
+    search_fields = ('$name',)
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
     pagination_class = None
